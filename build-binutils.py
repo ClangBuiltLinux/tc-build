72c09d11e57e24ff4dcd113f996befbe23a44f5c
Nathan Chancellor <natechancellor@gmail.com>
2019-04-15 23:45:30 -0700

    build-binutils.py: Implement parse_parameters
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- build-binutils.py
+++ build-binutils.py
@@ -1,11 +1,36 @@
 #!/usr/bin/env python
 
+import argparse
 import os
+
+
+def parse_parameters():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-I", "--install-folder",
+                        help="""
+                        By default, the script will create a "usr" folder in the same folder as this script
+                        and install binutils there. If you'd like to have it installed somewhere else, pass
+                        it to this parameter. This can either be an absolute or relative path.
+
+                        Example: ~/binutils
+                        """, type=str, default=os.getcwd() + "/usr")
+    parser.add_argument("-t", "--targets",
+                        help="""
+                        The script can build binutils targeting arm-linux-gnueabi, aarch64-linux-gnu,
+                        powerpc-linux-gnu, powerpc64le-linux-gnu, and x86_64-linux-gnu (host if on x86_64).
+
+                        You can either pass the full target or just the first part (arm, aarch64, etc) or all
+                        if you want to build all targets (which is the default).
+
+                        Example: all, aarch64, arm-linux-gnueabi
+                        """, default="all", nargs="+")
+    return parser.parse_args()
 
 
 def main():
     root = os.path.dirname(os.path.realpath(__file__))
     os.chdir(root)
+    args = parse_parameters()
 
 
 if __name__ == '__main__':
