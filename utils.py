4be375ae1a409c16404215700ee889adb7ac1049
Nathan Chancellor <natechancellor@gmail.com>
2019-04-16 21:03:44 -0700

    utils.py: Add comments
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- utils.py
+++ utils.py
@@ -10,20 +10,32 @@ import os
 import shutil
 import subprocess
 
+
+# The latest stable version of binutils
 def current_binutils():
     return "binutils-2.32"
 
+
+# Download the latest stable version of binutils
 def download_binutils(root):
     binutils = current_binutils()
     p = pathlib.Path.joinpath(root, binutils)
     if not p.is_dir():
+        # Remove any previous copies of binutils
         for entity in root.glob('binutils*'):
             if entity.is_dir():
                 shutil.rmtree(entity.as_posix())
             else:
                 entity.unlink()
+
+        # Download the tarball
         binutils_tarball = pathlib.Path.joinpath(root, binutils + ".tar.gz")
         subprocess.run(["curl", "-LSs", "-o", binutils_tarball.as_posix(), "https://ftp.gnu.org/gnu/binutils/" + binutils_tarball.name], check=True)
+
+        # Check the sha256sum of the downloaded package with a known good one
+        # To regenerate the sha256sum, download the .tar.gz and .tar.gz.sig files
+        # $ gpg --verify *.tar.gz.sig *.tar.gz
+        # $ sha256sum *.tar.gz
         file_hash = hashlib.sha256()
         with binutils_tarball.open("rb") as f:
             while True:
@@ -33,10 +45,13 @@ def download_binutils(root):
                 file_hash.update(data)
         if file_hash.hexdigest() != "9b0d97b3d30df184d302bced12f976aa1e5fbf4b0be696cdebc6cca30411a46e":
             raise RuntimeError("binutils sha256sum does not match known good one!")
+
+        # Extract the tarball then remove it
         subprocess.run(["tar", "-xzf", binutils_tarball.name], check=True)
         binutils_tarball.unlink()
 
 
+# Print a fancy header
 def header(string):
     print('\033[01;31m')
     for x in range(0, len(string) + 6):
