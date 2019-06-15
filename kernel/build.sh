6a26f179956d965ac07fc703d4f3313487fb9ffa
Nathan Chancellor <natechancellor@gmail.com>
2019-06-15 07:58:28 -0700

    build-llvm.py + kernel/build.sh: Pass in the targets that LLVM supports
    We need the kernel build script to know what backends the compiler
    supports, otherwise the build will fail because of unsupported backends.
    
    Reported-by: Harsh Shandilya <msfjarvis@gmail.com>
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- kernel/build.sh
+++ kernel/build.sh
@@ -4,18 +4,44 @@
 TC_BLD=$(cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"/.. && pwd)
 [[ -z ${TC_BLD} ]] && exit 1
 
+# Parse parameters
+while (( ${#} )); do
+    case ${1} in
+        "-p"|"--path-override")
+            shift
+            PATH_OVERRIDE=${1} ;;
+        "-s"|"--src-folder")
+            shift
+            SRC_FOLDER=${1} ;;
+        "-t"|"--targets")
+            shift
+            IFS=";" read -ra LLVM_TARGETS <<< "${1}"
+            # Convert LLVM targets into GNU triples
+            for LLVM_TARGET in "${LLVM_TARGETS[@]}"; do
+                case ${LLVM_TARGET} in
+                    "AArch64") TARGETS=( "${TARGETS[@]}" "aarch64-linux-gnu" ) ;;
+                    "ARM") TARGETS=( "${TARGETS[@]}" "arm-linux-gnueabi" ) ;;
+                    "PowerPC") TARGETS=( "${TARGETS[@]}" "powerpc-linux-gnu" "powerpc64le-linux-gnueabi" ) ;;
+                    "X86") TARGETS=( "${TARGETS[@]}" "x86_64-linux-gnu" ) ;;
+                esac
+            done
+    esac
+    shift
+done
+[[ -z ${TARGETS} ]] && TARGETS=( "arm-linux-gnueabi" "aarch64-linux-gnu" "powerpc-linux-gnu" "powerpc64le-linux-gnu" "x86_64-linux-gnu" )
+
 # Add the default install bin folder to PATH for binutils
 # Add the stage 2 bin folder to PATH for the instrumented clang
-for FOLDER in ${TC_BLD}/install/bin ${TC_BLD}/build/llvm/stage2/bin; do
-    [[ -d ${FOLDER} ]] && export PATH=${FOLDER}:${PATH}
+for BIN_FOLDER in ${TC_BLD}/install/bin ${TC_BLD}/build/llvm/stage2/bin; do
+    export PATH=${BIN_FOLDER}:${PATH}
 done
 
 # If the user wants to add another folder to PATH, they can do it with the PATH_OVERRIDE variable
 [[ -n ${PATH_OVERRIDE} ]] && export PATH=${PATH_OVERRIDE}:${PATH}
 
-# A kernel folder can be supplied for testing the script
-if [[ ${#} -gt 0 ]]; then
-    cd "${1}" || exit 1
+# A kernel folder can be supplied via '-f' for testing the script
+if [[ -n ${SRC_FOLDER} ]]; then
+    cd "${SRC_FOLDER}" || exit 1
 else
     LINUX=linux-5.1
     LINUX_TARBALL=${TC_BLD}/kernel/${LINUX}.tar.gz
@@ -32,23 +58,28 @@ else
 fi
 
 # Check for all binutils and build them if necessary
-TARGETS=()
-for PREFIX in arm-linux-gnueabi aarch64-linux-gnu powerpc-linux-gnu powerpc64le-linux-gnu; do
-    command -v "${PREFIX}"-as &>/dev/null || TARGETS=( "${TARGETS[@]}" "${PREFIX}" )
+BINUTILS_TARGETS=()
+for PREFIX in "${TARGETS[@]}"; do
+    # We assume an x86_64 host, should probably make this more generic in the future
+    if [[ ${PREFIX} = "x86_64-linux-gnu" ]]; then
+        COMMAND=as
+    else
+        COMMAND="${PREFIX}"-as
+    fi
+    command -v "${COMMAND}" &>/dev/null || BINUTILS_TARGETS=( "${BINUTILS_TARGETS[@]}" "${PREFIX}" )
 done
-command -v as &>/dev/null || TARGETS=( "${TARGETS[@]}" host )
-[[ -n "${TARGETS[*]}" ]] && { "${TC_BLD}"/build-binutils.py -t "${TARGETS[@]}" || exit ${?}; }
+[[ -n "${BINUTILS_TARGETS[*]}" ]] && { "${TC_BLD}"/build-binutils.py -t "${BINUTILS_TARGETS[@]}" || exit ${?}; }
 
 # SC2191: The = here is literal. To assign by index, use ( [index]=value ) with no spaces. To keep as literal, quote it.
 # shellcheck disable=SC2191
 MAKE=( make -j"$(nproc)" CC=clang HOSTCC=clang HOSTLD=ld.lld O=out )
 
-time "${MAKE[@]}" ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- LD=ld.lld distclean defconfig zImage modules || exit ${?}
-
-time "${MAKE[@]}" ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- LD=ld.lld distclean defconfig Image.gz modules || exit ${?}
-
-time "${MAKE[@]}" ARCH=powerpc CROSS_COMPILE=powerpc-linux-gnu- distclean ppc44x_defconfig zImage modules || exit ${?}
-
-time "${MAKE[@]}" ARCH=powerpc CROSS_COMPILE=powerpc64le-linux-gnu- distclean powernv_defconfig zImage.epapr modules || exit ${?}
-
-time "${MAKE[@]}" LD=ld.lld O=out distclean defconfig bzImage modules || exit ${?}
+for TARGET in "${TARGETS[@]}"; do
+    case ${TARGET} in
+        "arm-linux-gnueabi") time "${MAKE[@]}" ARCH=arm CROSS_COMPILE=${TARGET}- LD=ld.lld distclean defconfig zImage modules || exit ${?} ;;
+        "aarch64-linux-gnu") time "${MAKE[@]}" ARCH=arm64 CROSS_COMPILE=${TARGET}- LD=ld.lld distclean defconfig Image.gz modules || exit ${?} ;;
+        "powerpc-linux-gnu") time "${MAKE[@]}" ARCH=powerpc CROSS_COMPILE=${TARGET}- distclean ppc44x_defconfig zImage modules || exit ${?} ;;
+        "powerpc64le-linux-gnu") time "${MAKE[@]}" ARCH=powerpc CROSS_COMPILE=${TARGET}- distclean powernv_defconfig zImage.epapr modules || exit ${?} ;;
+        "x86_64-linux-gnu") time "${MAKE[@]}" LD=ld.lld O=out distclean defconfig bzImage modules || exit ${?} ;;
+    esac
+done
