5d4fc69c453e679ec385cdd3edf4e71f4137557f
Nathan Chancellor <natechancellor@gmail.com>
2019-07-23 17:39:45 -0700

    build-llvm.py: Add '--no-ccache' flag
    This can be useful for benchmarking how long certain build
    configurations take without having to wipe a built up cache.
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- build-llvm.py
+++ build-llvm.py
@@ -149,6 +149,12 @@ def parse_parameters(root_folder):
 
                         """),
                         action="store_true")
+    parser.add_argument("--no-ccache",
+                        help=textwrap.dedent("""\
+                        Don't enable LLVM_CCACHE_BUILD. Useful for benchmarking clean builds.
+
+                        """),
+                        action="store_true")
     parser.add_argument("-p",
                         "--projects",
                         help=textwrap.dedent("""\
@@ -573,7 +579,7 @@ def stage_specific_cmake_defines(args, dirs, stage):
 
     # Use ccache for the stage 1 build as it will usually be done with a consistent
     # compiler and won't need a full rebuild very often
-    if stage == 1 and shutil.which("ccache") is not None:
+    if stage == 1 and not args.no_ccache and shutil.which("ccache") is not None:
         defines['LLVM_CCACHE_BUILD'] = 'ON'
 
     if bootstrap_stage(args, stage):
