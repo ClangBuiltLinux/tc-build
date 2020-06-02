0b2ca590eefae1a3d91ee849d7583ee8ee1b9178
Nathan Chancellor <natechancellor@gmail.com>
2020-06-02 12:31:18 -0700

    kernel: Update to Linux 5.7
    Qualified with https://github.com/nathanchance/llvm-kernel-testing for
    LLVM 10.0.1 and 11.0.0.
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- kernel/build.sh
+++ kernel/build.sh
@@ -64,7 +64,7 @@ ${PGO:=false} && export PATH=${BUILD_FOLDER:=${TC_BLD}/build/llvm}/stage2/bin:${
 if [[ -n ${SRC_FOLDER} ]]; then
     cd "${SRC_FOLDER}" || exit 1
 else
-    LINUX=linux-5.6
+    LINUX=linux-5.7
     LINUX_TARBALL=${TC_BLD}/kernel/${LINUX}.tar.xz
     LINUX_PATCH=${TC_BLD}/kernel/${LINUX}-${CONFIG_TARGET}.patch
 
