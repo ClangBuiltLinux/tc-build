ClangBuiltLinux/tc-build

commit 6d2853aaa286612181bc611cbf35b6cf674b5044

build-llvm.py: Add '--full-toolchain'
When doing upstream LLVM development or using the toolchain produced by
the script system-wide,it is advantageous to build as much of the code
as possible, rather than a subset like the script currently does.

Introduce '--full-toolchain', which stops the script from disabling
certain projects, targets, and configuration options. The '--projects',
'--targets', and '--defines' options are still respected so this
behavior can be more finely controlled.

Signed-off-by: Nathan Chancellor <nathan@kernel.org>

build-llvm.py
@@ -155,6 +155,21 @@ def parse_parameters(root_folder):
 
                         """),
                         nargs="+")
+    parser.add_argument("-f",
+                        "--full-toolchain",
+                        help=textwrap.dedent("""\
+                        By default, the script tunes LLVM for building the Linux kernel by disabling several
+                        projects, targets, and configuration options, which speeds up build times but limits
+                        how the toolchain could be used.
+
+                        With this option, all projects and targets are enabled and the script tries to avoid
+                        unnecessarily turning off configuration options. The '--projects' and '--targets' options
+                        to the script can still be used to change the list of projects and targets. This is
+                        useful when using the script to do upstream LLVM development or trying to use LLVM as a
+                        system-wide toolchain.
+
+                        """),
+                        action="store_true")
     parser.add_argument("-i",
                         "--incremental",
                         help=textwrap.dedent("""\
@@ -236,8 +251,8 @@ def parse_parameters(root_folder):
     parser.add_argument("-p",
                         "--projects",
                         help=textwrap.dedent("""\
-                        Currently, the script only enables the clang, compiler-rt, and lld folders in LLVM. If
-                        you would like to override this, you can use this parameter and supply a list that is
+                        Currently, the script only enables the clang, compiler-rt, lld, and polly folders in LLVM.
+                        If you would like to override this, you can use this parameter and supply a list that is
                         supported by LLVM_ENABLE_PROJECTS.
 
                         See step #5 here: https://llvm.org/docs/GettingStarted.html#getting-started-quickly-a-summary
@@ -245,8 +260,7 @@ def parse_parameters(root_folder):
                         Example: -p \"clang;lld;libcxx\"
 
                         """),
-                        type=str,
-                        default="clang;compiler-rt;lld;polly")
+                        type=str)
     opt_options.add_argument("--pgo",
                              help=textwrap.dedent("""\
                         Build the final compiler with Profile Guided Optimization, which can often improve compile
@@ -304,7 +318,6 @@ def parse_parameters(root_folder):
 
                         """),
                         action="store_true")
-    # yapf: disable
     parser.add_argument("-t",
                         "--targets",
                         help=textwrap.dedent("""\
@@ -318,9 +331,7 @@ def parse_parameters(root_folder):
                         Example: -t "AArch64;X86"
 
                         """),
-                        type=str,
-                        default="AArch64;ARM;BPF;Mips;PowerPC;RISCV;SystemZ;X86")
-    # yapf: enable
+                        type=str)
     clone_options.add_argument("--use-good-revision",
                                help=textwrap.dedent("""\
                         By default, the script updates LLVM to the latest tip of tree revision, which may at times be
@@ -650,10 +661,9 @@ def instrumented_stage(args, stage):
     return args.pgo and stage == 2
 
 
-def base_cmake_defines():
+def slim_cmake_defines():
     """
-    Generate base cmake defines, which will always be present, regardless of
-    user input and stage
+    Generate a set of cmake defines to slim down the LLVM toolchain
     :return: A set of defines
     """
     # yapf: disable
@@ -672,8 +682,6 @@ def base_cmake_defines():
         'LLVM_ENABLE_BINDINGS': 'OFF',
         # Don't build Ocaml documentation
         'LLVM_ENABLE_OCAMLDOC': 'OFF',
-        # Removes system dependency on terminfo and almost every major clang provider turns this off
-        'LLVM_ENABLE_TERMINFO': 'OFF',
         # Don't build clang-tools-extras to cut down on build targets (about 400 files or so)
         'LLVM_EXTERNAL_CLANG_TOOLS_EXTRA_SOURCE_DIR': '',
         # Don't include documentation build targets because it is available on the web
@@ -768,9 +776,69 @@ def distro_cmake_defines():
     return defines
 
 
-def project_target_cmake_defines(args, stage):
+def project_cmake_defines(args, stage):
     """
-    Generate project and target cmake defines, which change depending on what
+    Generate lists of projects, depending on whether a full or
+    kernel-focused LLVM build is being done and the stage
+    :param args: The args variable generated by parse_parameters
+    :param stage: What stage we are at
+    :return: A set of defines
+    """
+    defines = {}
+
+    if args.full_toolchain:
+        if args.projects:
+            projects = args.projects
+        else:
+            projects = "all"
+    else:
+        if bootstrap_stage(args, stage):
+            projects = "clang;lld"
+            if args.pgo:
+                projects += ';compiler-rt'
+        else:
+            if instrumented_stage(args, stage):
+                projects = "clang;lld"
+            else:
+                projects = "clang;compiler-rt;lld;polly"
+
+    defines['LLVM_ENABLE_PROJECTS'] = projects
+
+    if "compiler-rt" in projects:
+        if not args.full_toolchain:
+            # Don't build libfuzzer when compiler-rt is enabled, it invokes cmake again and we don't use it
+            defines['COMPILER_RT_BUILD_LIBFUZZER'] = 'OFF'
+            # We only use compiler-rt for the sanitizers, disable some extra stuff we don't need
+            # Chromium OS also does this: https://crrev.com/c/1629950
+            defines['COMPILER_RT_BUILD_BUILTINS'] = 'OFF'
+            defines['COMPILER_RT_BUILD_CRT'] = 'OFF'
+            defines['COMPILER_RT_BUILD_XRAY'] = 'OFF'
+        # We don't need the sanitizers for the stage 1 bootstrap
+        if bootstrap_stage(args, stage):
+            defines['COMPILER_RT_BUILD_SANITIZERS'] = 'OFF'
+
+    return defines
+
+
+def get_targets(args):
+    """
+    Gets the list of targets for cmake and kernel/build.sh
+    :param args: The args variable generated by parse_parameters
+    :return: A string of targets suitable for cmake or kernel/build.sh
+    """
+    if args.targets:
+        targets = args.targets
+    elif args.full_toolchain:
+        targets = "all"
+    else:
+        targets = "AArch64;ARM;BPF;Mips;PowerPC;RISCV;SystemZ;X86"
+
+    return targets
+
+
+def target_cmake_defines(args, stage):
+    """
+    Generate target cmake define, which change depending on what
     stage we are at
     :param args: The args variable generated by parse_parameters
     :param stage: What stage we are at
@@ -779,34 +847,11 @@ def project_target_cmake_defines(args, stage):
     defines = {}
 
     if bootstrap_stage(args, stage):
-        projects = "clang;lld"
-        if args.pgo:
-            projects += ';compiler-rt'
         targets = "host"
     else:
-        if instrumented_stage(args, stage):
-            projects = "clang;lld"
-        else:
-            projects = args.projects
-        targets = args.targets
+        targets = get_targets(args)
 
-    # The projects to build
-    defines['LLVM_ENABLE_PROJECTS'] = projects
-
-    # The architectures to build backends for
     defines['LLVM_TARGETS_TO_BUILD'] = targets
-
-    if "compiler-rt" in projects:
-        # Don't build libfuzzer when compiler-rt is enabled, it invokes cmake again and we don't use it
-        defines['COMPILER_RT_BUILD_LIBFUZZER'] = 'OFF'
-        # We only use compiler-rt for the sanitizers, disable some extra stuff we don't need
-        # Chromium OS also does this: https://crrev.com/c/1629950
-        defines['COMPILER_RT_BUILD_BUILTINS'] = 'OFF'
-        defines['COMPILER_RT_BUILD_CRT'] = 'OFF'
-        defines['COMPILER_RT_BUILD_XRAY'] = 'OFF'
-        # We don't need the sanitizers for the stage 1 bootstrap
-        if bootstrap_stage(args, stage):
-            defines['COMPILER_RT_BUILD_SANITIZERS'] = 'OFF'
 
     return defines
 
@@ -828,11 +873,12 @@ def stage_specific_cmake_defines(args, dirs, stage):
 
     if bootstrap_stage(args, stage):
         # Based on clang/cmake/caches/Apple-stage1.cmake
+        defines.update(slim_cmake_defines())
         defines['CMAKE_BUILD_TYPE'] = 'Release'
+        defines['LLVM_BUILD_UTILS'] = 'OFF'
         defines['LLVM_ENABLE_BACKTRACES'] = 'OFF'
         defines['LLVM_ENABLE_WARNINGS'] = 'OFF'
         defines['LLVM_INCLUDE_TESTS'] = 'OFF'
-        defines['LLVM_INCLUDE_UTILS'] = 'OFF'
     else:
         # https://llvm.org/docs/CMake.html#frequently-used-cmake-variables
         defines['CMAKE_BUILD_TYPE'] = args.build_type
@@ -887,9 +933,11 @@ def build_cmake_defines(args, dirs, env_vars, stage):
     :param stage: What stage we are at
     :return: A set of defines
     """
+    defines = {}
 
-    # Get base defines, which don't depend on any user inputs
-    defines = base_cmake_defines()
+    # Get slim defines if we are not building a full toolchain
+    if not args.full_toolchain:
+        defines.update(slim_cmake_defines())
 
     # Add compiler/linker defines, which change based on stage
     defines.update(cc_ld_cmake_defines(dirs, env_vars, stage))
@@ -898,7 +946,8 @@ def build_cmake_defines(args, dirs, env_vars, stage):
     defines.update(distro_cmake_defines())
 
     # Add project and target defines, which change based on stage
-    defines.update(project_target_cmake_defines(args, stage))
+    defines.update(project_cmake_defines(args, stage))
+    defines.update(target_cmake_defines(args, stage))
 
     # Add other stage specific defines
     defines.update(stage_specific_cmake_defines(args, dirs, stage))
@@ -906,6 +955,9 @@ def build_cmake_defines(args, dirs, env_vars, stage):
     # Add the vendor string if necessary
     if args.clang_vendor:
         defines['CLANG_VENDOR'] = args.clang_vendor
+
+    # Removes system dependency on terminfo to keep the dynamic library dependencies slim
+    defines['LLVM_ENABLE_TERMINFO'] = 'OFF'
 
     return defines
 
@@ -1032,7 +1084,8 @@ def generate_pgo_profiles(args, dirs):
     build_sh = [
         dirs.root_folder.joinpath("kernel", "build.sh"), '-b',
         dirs.build_folder, '--pgo',
-        str(args.pgo).lower(), '-t', args.targets
+        str(args.pgo).lower(), '-t',
+        get_targets(args)
     ]
     if dirs.linux_folder:
         build_sh += ['-s', dirs.linux_folder.as_posix()]

kernel/build.sh
@@ -50,7 +50,7 @@ while ((${#})); do
     esac
     shift
 done
-[[ -z ${TARGETS[*]} ]] && TARGETS=(
+[[ -z ${TARGETS[*]} || ${TARGETS[*]} = "all" ]] && TARGETS=(
     "arm-linux-gnueabi"
     "aarch64-linux-gnu"
     "mipsel-linux-gnu"

commit 406338e1d16411c2424dc102967c0ea9a009d5f5

build-llvm.py: Fix handling of '--projects'
Turns out I broke this in commit 6d2853a ("build-llvm.py: Add
'--full-toolchain'") because I never handled the case of '--projects'
being specified. Make the if statements a little simpler and handle
'--projects' again.

Signed-off-by: Nathan Chancellor <nathan@kernel.org>

build-llvm.py
@@ -843,11 +843,12 @@ def project_cmake_defines(args, stage):
             projects = "clang;lld"
             if args.pgo:
                 projects += ';compiler-rt'
+        elif instrumented_stage(args, stage):
+            projects = "clang;lld"
+        elif args.projects:
+            projects = args.projects
         else:
-            if instrumented_stage(args, stage):
-                projects = "clang;lld"
-            else:
-                projects = "clang;compiler-rt;lld;polly"
+            projects = "clang;compiler-rt;lld;polly"
 
     defines['LLVM_ENABLE_PROJECTS'] = projects
 