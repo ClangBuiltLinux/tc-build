7a6415164747390458547ac2cf5149e9930ffcd9
Nathan Chancellor <nathan@kernel.org>
2021-09-16 08:44:33 -0700

    kernel/build.sh: Do not attempt to print GNU as version for Hexagon
    It does not exist, as noted above this section.
    
    Fixes: e3851eb ("kernel/build.sh: Support Hexagon")
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- kernel/build.sh
+++ kernel/build.sh
@@ -155,6 +155,7 @@ function print_tc_info() {
         echo
         case ${PREFIX} in
             x86_64-linux-gnu) as --version ;;
+            hexagon-linux-gnu) ;;
             *) "${PREFIX}"-as --version ;;
         esac
     done
