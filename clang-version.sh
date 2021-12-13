ecc0cbcfb2ec42d67ca268bb34279f2e8191e13f
Nathan Chancellor <nathan@kernel.org>
2021-12-13 15:15:27 -0700

    clang-version.sh: Import new version based on cc-version.sh
    clang-version.sh was rewritten upstream as cc-version.sh, which is 3x as
    fast because it invokes the compiler once instead of three times.
    
    $ hyperfine \
        --runs 200 \
        --warmup 20 \
        "./clang-version.sh clang" \
        "./cc-version.sh clang"
    Benchmark 1: ./clang-version.sh clang
      Time (mean ± σ):     116.5 ms ±   1.9 ms    [User: 66.7 ms, System: 58.8 ms]
      Range (min … max):   106.5 ms … 119.5 ms    200 runs
    
    Benchmark 2: ./cc-version.sh clang
      Time (mean ± σ):      32.8 ms ±   1.0 ms    [User: 18.2 ms, System: 15.6 ms]
      Range (min … max):    27.0 ms …  34.7 ms    200 runs
    
    Summary
      './cc-version.sh clang' ran
        3.56 ± 0.12 times faster than './clang-version.sh clang'
    
    This script will be used in at least one other place going forward so it
    makes sense to update it to get the free performance gains (no matter
    how small they are).
    
    It has been stripped of anything not related to getting clang's version
    as a five or six digit number so it looks a lot different from upstream.
    
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- clang-version.sh
+++ clang-version.sh
@@ -1,19 +1,26 @@
 #!/bin/sh
+# shellcheck disable=SC2046,SC2086
 # SPDX-License-Identifier: GPL-2.0
 #
-# clang-version clang-command
-#
-# Print the compiler version of `clang-command' in a 5 or 6-digit form
-# such as `50001' for clang-5.0.1 etc.
+# Print clang's version in a 5 or 6-digit form.
 
-compiler="$*"
+set -e
 
-if ! ($compiler --version | grep -q clang); then
-    echo 0
-    exit 1
-fi
+# Print the compiler name and some version components.
+get_compiler_info() {
+    cat <<-EOF | "$@" -E -P -x c - 2>/dev/null
+	__clang_major__  __clang_minor__  __clang_patchlevel__
+	EOF
+}
 
-MAJOR=$(echo __clang_major__ | $compiler -E -x c - | tail -n 1)
-MINOR=$(echo __clang_minor__ | $compiler -E -x c - | tail -n 1)
-PATCHLEVEL=$(echo __clang_patchlevel__ | $compiler -E -x c - | tail -n 1)
-printf "%d%02d%02d\\n" "$MAJOR" "$MINOR" "$PATCHLEVEL"
+# Convert the version string x.y.z to a canonical 5 or 6-digit form.
+get_canonical_version() {
+    IFS=.
+    set -- $1
+    echo $((10000 * $1 + 100 * $2 + $3))
+}
+
+# $@ instead of $1 because multiple words might be given, e.g. CC="ccache gcc".
+set -- $(get_compiler_info "$@")
+
+get_canonical_version $1.$2.$3
