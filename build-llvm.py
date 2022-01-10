d8e519692ad67bbf7f8af6ec4267cb0f270f9f47
Nathan Chancellor <nathan@kernel.org>
2022-01-10 08:49:14 -0700

    build-llvm.py: Update known good revision
    Qualified with https://github.com/nathanchance/llvm-kernel-testing.
    
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- build-llvm.py
+++ build-llvm.py
@@ -16,7 +16,7 @@ import urllib.request as request
 from urllib.error import URLError
 
 # This is a known good revision of LLVM for building the kernel
-GOOD_REVISION = '4b1fde8a2b681dad2ce0c082a5d6422caa06b0bc'
+GOOD_REVISION = '08f70adedb775ce6d41a1f8ad75c4bac225efb5b'
 
 
 class Directories:
