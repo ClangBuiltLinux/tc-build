8db2eebd436e946fee540437eeaeca9374092be5
Nathan Chancellor <nathan@kernel.org>
2026-02-09 12:59:43 -0700

    build-llvm.py: Update GOOD_REVISION to 2a2a394215b38631588d504d2b671df13370395b
    This has been lightly qualified on an aarch64 and x86_64 host with the
    environment and configuration that I build the kernel.org toolchains
    with.
    
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- build-llvm.py
+++ build-llvm.py
@@ -22,7 +22,7 @@ except ImportError:
     BOOL_ARGS = {'action': 'store_true'}
 
 # This is a known good revision of LLVM for building the kernel
-GOOD_REVISION = '81c5d468cf00d6e41112fba6c89d6c40013bcbda'
+GOOD_REVISION = '2a2a394215b38631588d504d2b671df13370395b'
 
 # The version of the Linux kernel that the script downloads if necessary
 DEFAULT_KERNEL_FOR_PGO = (6, 19, 0)
